37a95c70dc9a211f6724f04886878526d75443a3
👑➕🍋 <50390200+eee555@users.noreply.github.com>
2026-06-23 17:37:46 +0800

    ci: 修复错误

--- build.bat
+++ build.bat
@@ -19,7 +19,7 @@ pyinstaller --noconfirm --name metaminsweeper --windowed --distpath %OUT% ^
 
 echo.
 echo [2/3] plugin_manager.exe
-pyinstaller --noconfirm --name plugin_manager --windowed --icon src/media/cat.ico --hidden-import sqlite3 --hidden-import code --hidden-import xmlrpc --hidden-import xmlrpc.server --hidden-import xmlrpc.client --hidden-import http.server --hidden-import socketserver --hidden-import email --hidden-import email.utils --distpath %OUT% src\plugin_manager\_run.py
+pyinstaller --noconfirm --name plugin_manager --windowed --icon src/media/cat.ico --add-data "src/plugins;plugins" --add-data "src/shared_types;shared_types" --hidden-import sqlite3 --hidden-import code --hidden-import xmlrpc --hidden-import xmlrpc.server --hidden-import xmlrpc.client --hidden-import http.server --hidden-import socketserver --hidden-import email --hidden-import email.utils --hidden-import requests --hidden-import Crypto.Cipher.AES --hidden-import Crypto.Random --distpath %OUT% src\plugin_manager\_run.py
 
 echo.
 echo [3/3] Copy resources to metaminsweeper\
@@ -27,9 +27,9 @@ copy /y "%OUT%\plugin_manager\plugin_manager.exe" "%DEST%\"
 xcopy /e /y /i "%OUT%\plugin_manager\_internal" "%DEST%\_internal" >nul
 xcopy /e /y /i "src\plugin_sdk" "%DEST%\plugin_sdk" >nul
 xcopy /e /y /i "src\plugin_manager" "%DEST%\plugin_manager" >nul
-xcopy /e /y /i "src\plugins" "%DEST%\plugins" >nul
 xcopy /e /y /i "src\shared_types" "%DEST%\shared_types" >nul
 if exist "%DEST%\plugin_manager\_run.py" del /f /q "%DEST%\plugin_manager\_run.py"
+mkdir "%DEST%\user_plugins" >nul 2>&1
 
 echo [4/4] Copy debugpy from venv to _internal\
 set SP=.venv\Lib\site-packages
